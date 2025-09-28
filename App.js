964d191bec4cbf2b45265c1d367310856b9e4ce8
Vaibhav Gawas <vaibhavgawas343@gmail.com>
2025-09-28 23:07:19 +0530

    feat(ui): add header with logo and navbar

--- App.js
+++ App.js
@@ -1,18 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Parent = () => React.createElement("div", { id: "parent" }, "This is a  React Project");
-const JsxComp = () => <h1 className="head">This is JSX</h1>;
 
-const HeadingComponent = () => {
+const Header = () => {
   return (
     <div>
-      <Parent/>
-      <JsxComp/>
+      <div className="header">  
+        <div className="logo-container">
+          <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All" />
+        </div>
+
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+const ResturantCard = () => {
+  return (
+    <div className="res-card">
+      <img className="res-logo" alt ="resturant-logo" src="https://b.zmtcdn.com/data/pictures/3/19750683/987d86719049d6c9aa9667ae9ad9d2bc_o2_featured_v2.jpg"/>
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4> 4.5 stars</h4>
+      <h4> 30 mins</h4>
+    </div>
+  );
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"> Seach</div>
+      <div className="res-container"> 
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+        <ResturantCard/>
+      </div>
+    </div>
+  );
+}
+
+
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header/>
+      <Body/>
     </div>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
