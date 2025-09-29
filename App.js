d83bec9479c9384789414596532d96cfc66715ba
Vaibhav Gawas <vaibhavgawas343@gmail.com>
2025-09-29 23:09:22 +0530

    feat(ui): implement body layout with card components

--- App.js
+++ App.js
@@ -24,14 +24,15 @@ const Header = () => {
   );
 }
 
-const ResturantCard = () => {
+const ResturantCard = (props) => {
+  const {resName, cusine, rating, time} = props;
   return (
     <div className="res-card">
       <img className="res-logo" alt ="resturant-logo" src="https://b.zmtcdn.com/data/pictures/3/19750683/987d86719049d6c9aa9667ae9ad9d2bc_o2_featured_v2.jpg"/>
-      <h3>Meghana Foods</h3>
-      <h4>Biryani, North Indian, Asian</h4>
-      <h4> 4.5 stars</h4>
-      <h4> 30 mins</h4>
+      <h3>{resName}</h3>
+      <h4>{cusine}</h4>
+      <h4>{rating}</h4>
+      <h4>{time}</h4>
     </div>
   );
 }
@@ -41,20 +42,9 @@ const Body = () => {
     <div className="body">
       <div className="search"> Seach</div>
       <div className="res-container"> 
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
+        <ResturantCard  resName="Meghana Foods" cusine="Biryani, North Indian, Asian" rating="4.5 stars" time="30 mins"/>
+        <ResturantCard  resName="KFC" cusine="Chiken wings,kebab" rating="4.3 stars" time="33 mins"/>
+        
       </div>
     </div>
   );
